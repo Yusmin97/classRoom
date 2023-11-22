4ea508462a31f52b7e9089c295e42b2093b938c0
Yusmin97 <tmdamsi97@gmail.com>
2023-11-22 10:48:00 +0900

    challenge 2 작성

---
+++ back-end-basic-Node.js/challenge-2/challenge-2.js
@@ -0,0 +1,16 @@
+function challengeTwo(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - i - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++){
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+console.log(challengeTwo(unsortedArray)); // 출력; [11, 12, 22, 25, 34, 64, 90]
